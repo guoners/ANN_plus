e943bf043fcb9b8385fb7cd5fe83030a286777e4
Nur3e <guoners@163.com>
2017-07-18 21:01:24 +0800

    project file for notepadplus of MLP

---
+++ project.v
@@ -0,0 +1,50 @@
+<NotepadPlus>
+    <Project name="ANN_plus">
+        <Folder name="neuron">
+            <Folder name="top">
+                <File name="neuron\rtl\neuron.v" />
+            </Folder>
+            <Folder name="df">
+                <File name="neuron\rtl\df.v" />
+                <File name="neuron\rtl\df_cnt.v" />
+                <File name="neuron\rtl\df_mux.v" />
+                <File name="neuron\rtl\df_reg.v" />
+                <File name="neuron\rtl\df_wram.v" />
+            </Folder>
+            <Folder name="mu">
+                <File name="neuron\rtl\mu.v" />
+                <File name="neuron\rtl\mu_reg.v" />
+                <File name="neuron\rtl\Mult.v" />
+            </Folder>
+            <Folder name="ac">
+                <File name="neuron\rtl\ac.v" />
+                <File name="neuron\rtl\ADDer.v" />
+                <File name="neuron\rtl\ac_reg.v" />
+            </Folder>
+            <Folder name="mac_ctrl">
+                <File name="neuron\rtl\mac_ctrl.v" />
+            </Folder>
+            <Folder name="sig">
+                <File name="neuron\rtl\sig.v" />
+            </Folder>
+            <Folder name="include">
+                <File name="neuron\include\stddef.h" />
+            </Folder>
+            <Folder name="data" />
+            <File name="neuron\rtl\note.v" />
+            <File name="testbench\rtl\stimulus_neuron.v" />
+        </Folder>
+        <Folder name="neuron_o">
+            <File name="neuron_o\rtl\df_cnt_o.v" />
+            <File name="neuron_o\rtl\df_mux_o.v" />
+            <File name="neuron_o\rtl\df_o.v" />
+            <File name="neuron_o\rtl\df_wram_o.v" />
+            <File name="neuron_o\rtl\mac_ctrl_o.v" />
+            <File name="neuron_o\rtl\neuron_o.v" />
+        </Folder>
+        <Folder name="net">
+            <File name="net\rtl\net.v" />
+            <File name="testbench\rtl\stimulus_net.v" />
+        </Folder>
+    </Project>
+</NotepadPlus>
